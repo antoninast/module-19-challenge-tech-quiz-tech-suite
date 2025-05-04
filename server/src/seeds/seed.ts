c6d6f647cdc38afa8d8ec2dca434d6d74e1b4be9
Antonina S <antoninad22@gmail.com>
2025-05-03 22:10:02 -0500

    upload working application

---
+++ server/src/seeds/seed.ts
@@ -0,0 +1,28 @@
+import db from '../config/connection.js';
+import { Question } from '../models/index.js'
+import cleanDB from './cleanDb.js';
+
+// import questionData from './pythonQuestions.json' assert{ type: 'json'};
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const jsonPath = path.join(__dirname, './pythonQuestions.json');
+
+const data = await readFile(jsonPath, 'utf-8');
+const questionData = JSON.parse(data);
+
+try {
+  await db();
+  await cleanDB();
+
+  // bulk create each model
+  await Question.insertMany(questionData);
+
+  console.log('Seeding completed successfully!');
+  process.exit(0);
+} catch (error) {
+  console.error('Error seeding database:', error);
+  process.exit(1);
+}
